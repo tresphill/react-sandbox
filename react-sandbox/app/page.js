b57361b786cb770c907c6761aa94eda2fc2b13df
tphill <tphill760@gmail.com>
2023-10-11 10:43:57 -0400

    original commit

---
+++ react-sandbox/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  )
+}
